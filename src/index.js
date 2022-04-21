011492c1e01816f4bfaf3de28c9713e202390794
Arturo Vargas <ruisu.vargas@gmail.com>
2022-04-21 16:46:40 -0500

    Finish first session of the React Course

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
+// 1. Import the React and ReactDOM libraries
 import React from 'react';
+// import ReactDOM from 'react-dom';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// 2. Create React Components
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+const App = () => {
+
+    const buttonText = {text: 'Submit'};
+    const style = {backgroundColor: 'blue', color: 'white'};
+            
+    const labelText = 'Enter name:'
+
+    return (
+        <div><label className="label" htmlFor="name">
+            {labelText}
+        </label>
+        <input id="name" type="text"/>
+        <button style={style}>
+            {buttonText.text}
+        </button>
+        </div>
+    );
+}
+
+
+// 3. Take the React Components and show it on the screen
+// Old Way:
+// ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.createRoot(document.querySelector('#root')).render(<App />);
